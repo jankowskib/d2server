23361470f52782d71f8d9289a498f4f0097c9de2
Bartosz J <thug1337@gmail.com>
2013-04-03 18:16:33 +0200

    Updated license to files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 d2warden
-========
+=========== 
+
+This is the d2gs 1.11b source package release.
+
+
+1.Directories:
+  config\		some config file samples
+  d2gs\			source code of d2gs
+  d2warden\		source code of d2warden
+  patch_d2server\	source code of patch_d2server, which will be used to patch the d2gs. It only copy the 1.exe code section into the d2server.dll.
+
--- d2warden-pvp/D2Warden.cpp
+++ d2warden-pvp/D2Warden.cpp
@@ -1,15 +1,26 @@
-//////////////////////////////////////////////////////////////////////
-//
-// marsgod - <marsgod@263.sina.com>
-// lolet - <thug1337@gmail.com>
-//
-//////////////////////////////////////////////////////////////////////
+/* ==========================================================
+ * d2warden
+ * https://github.com/lolet/d2warden
+ * ==========================================================
+ * Copyright 2008 marsgod
+ *			 2013 lolet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
 
-//#pragma comment(lib, "wsock32.lib")
 
 #include "stdafx.h"
 
-//#include <winsock.h>
 #include "global.h"
 #include "MD5.h"
 #include "rc4.h"
